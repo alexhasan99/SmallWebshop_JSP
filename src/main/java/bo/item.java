905aeaa86683da5f8240d00108658ffe9a2193f5
alexhasan99 <mohammad.hasan19999@gmail.com>
2023-10-03 17:06:59 +0200

    added some classes

--- src/main/java/bo/item.java
+++ src/main/java/bo/item.java
@@ -1,5 +1,9 @@
 package bo;
-
 public class item {
+
+    private String name;
+    private String id;
+    private String descr;
+
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>Hello Kurder!</h2>
 </body>
 </html>
